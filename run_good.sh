31e16f8ae9f461a28bbc2b74c443daef631914f1
ola <ola.falkiewicz_27@wp.pl>
2021-01-08 17:42:23 +0100

    run_good checks outputs

--- run_good.sh
+++ run_good.sh
@@ -2,7 +2,20 @@
 
 for f in $(find tests/good/ -name '*.lat')
 do
-	echo $f
     stack exec latc_x86_64 $f
-done
+    
+    exe=tests/good/$(basename $f .lat)
+    in=$exe.input
+    myout=$exe.myout
+    output=$exe.output
 
+    echo $exe
+    if [ -e $in ]
+    then
+        $exe < $in > $myout
+    else
+        $exe > $myout
+    fi
+    diff $myout $output
+
+done
